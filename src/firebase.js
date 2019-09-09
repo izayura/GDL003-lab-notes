03783eddf1f08db9d92d638f4591df303f8425e4
Gloria <izayura@gmail.com>
2019-09-09 17:48:15 -0500

    Se cambio api key de firebase por motivos de seguridad

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,10 @@
 export default{
-    apiKey: "AIzaSyAz1c-lVQ4fXTdqaBGvA5AnipYVQGKXy54",
+    apiKey: "AIzaSyBBkkGSIl36YaRWpopOzrDfpTS2xuXvXj0",
     authDomain: "remember-notes-361ff.firebaseapp.com",
     databaseURL: "https://remember-notes-361ff.firebaseio.com",
     projectId: "remember-notes-361ff",
-    storageBucket: "",
+    storageBucket: "remember-notes-361ff.appspot.com",
     messagingSenderId: "1001836723464",
     appId: "1:1001836723464:web:e4781cf197366e7c9f00bd"
+
 }
